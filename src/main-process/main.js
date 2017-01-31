b901ae75359ca5d53e1fe4a7ab5d9a8c284b72b4
Deepak Kannan <kannan.deepak@gmail.com>
2017-01-31 13:26:42 +0530

    Call scraper from http server

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -5,11 +5,12 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import url from 'url';
+import { spawn } from 'child_process';
+import { StringDecoder } from 'string_decoder';
 
 const http = require("http");
 const port = 8080;
 
-const outputFolder = path.join(app.getPath('userData'), "outputs");
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow = null;
@@ -75,6 +76,66 @@ app.on('activate', () => {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 
+function scrapeOnWindows(scraperPath) {
+  const outputPath = path.join(app.getPath('userData'), "outputs");
+  const casperPath = path.join(__dirname, "../casperjs/batchbin/casperjs.bat");
+  console.log(`===> outputPath: ${outputPath}`);
+  console.log(`===> casperPath: ${casperPath}`);
+  const bat = spawn('cmd.exe', ['/c', `${casperPath} ${scraperPath} --url=https://www.google.co.in --outputPath=${outputPath}` ]);
+
+  const stdoutDecoder = new StringDecoder('utf8');
+  const stderrDecoder = new StringDecoder('utf8');
+
+  bat.stdout.on('data', (data) => {
+    var text = stdoutDecoder.write(data);
+    console.log(text);
+  });
+
+  bat.stderr.on('data', (data) => {
+    var text = stderrDecoder.write(data);
+    console.log(text);
+  });
+
+  bat.on('exit', (code) => {
+    console.log(`Child exited with code ${code}`);
+  });
+}
+
+function scrapeOnDarwin(scraperPath) {
+  const outputPath = path.join(app.getPath('userData'), "outputs");
+  console.log(`===> outputPath: ${outputPath}`);
+  const bat = spawn("casperjs", [
+    scraperPath,
+    "--url=https://www.google.co.in",
+    `--outputPath=${outputPath}`
+  ]);
+
+  const stdoutDecoder = new StringDecoder('utf8');
+  const stderrDecoder = new StringDecoder('utf8');
+
+  bat.stdout.on('data', (data) => {
+    var text = stdoutDecoder.write(data);
+    console.log(text);
+  });
+
+  bat.stderr.on('data', (data) => {
+    var text = stderrDecoder.write(data);
+    console.log(text);
+  });
+
+  bat.on('exit', (code) => {
+    console.log(`Child exited with code ${code}`);
+  });
+}
+
+function scrape(scraperPath) {
+  if (process.platform === 'darwin') {
+    scrapeOnDarwin(scraperPath);
+  } else {
+    scrapeOnWindows(scraperPath);
+  }
+}
+
 http.createServer(function(request, response) {
   // Set CORS headers
 	response.setHeader('Access-Control-Allow-Origin', '*');
@@ -89,6 +150,10 @@ http.createServer(function(request, response) {
     response.writeHead(200, {
       'Content-Type': 'application/json'
     });
+
+    const scraperPath = path.join(__dirname, "../scrapers/cscrape.js");
+    scrape(scraperPath);
+
     setTimeout(() => {
       response.end(JSON.stringify({ message: 'test' }));
     }, 2000);
